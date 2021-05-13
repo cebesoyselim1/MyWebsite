eeaa52ecf8cb6a8c892ee5ade831dc5f326d8ed8
Selim Cebesoy <cebesoyselim1@gmail.com>
2021-05-13 10:01:04 +0300

    Styled and adnimated navbar with it's brand

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,52 @@
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+
+
+export class NavComponent implements OnInit {
+  @ViewChild('brand') brand:any;
+  knowledges:string[] = 
+  ["HTML","CSS","BOOTSTRAP","SCSS","JAVASCRIPT","ANGULAR"];
+  timer:number = 4000;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initBrandAnim();
+    setInterval(() => {
+      this.initBrandAnim();
+    },this.knowledges.length * this.timer)
+  }
+
+  toggleActive(toggler:any, navbarLinks:any){
+    toggler.classList.toggle("active");
+    navbarLinks.classList.toggle("active");
+  }
+
+  initBrandAnim(){
+    let indexPrev = 0;
+    this.knowledges.forEach((knowledge,index) => {
+      let arr = Array.from(knowledge);
+        arr.forEach((char, indexInner) => [
+          setTimeout(() => {
+            try{
+              if(index == indexPrev){
+                (this.brand as ElementRef).nativeElement.innerHTML = "";
+                indexPrev++;
+              }
+              (this.brand as ElementRef).nativeElement.innerHTML += char;
+            }catch(err){
+              console.log(err);
+            }
+          }, index * this.timer + (this.timer / arr.length) * indexInner)
+        ])
+    })
+  }
+
+
+}
